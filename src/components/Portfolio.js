562148ccd6f50cfe8075e6b5a620f2bd6a4e4952
Adam Olson <adamlsn@gmail.com>
2021-08-05 17:52:23 -0500

    changing portfolio to include latest project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,9 +4,9 @@ import Project from "./Project";
 function Portfolio(){
 
     const [projects] = useState([
+        {name: "Busted Brackets", url: "https://limitless-wildwood-05739.herokuapp.com/", github: "https://github.com/adamlsn/busted-brackets"},
         {name: "Craft Reviews", url: "https://review-crafts.herokuapp.com/", github: "https://github.com/mspierings96/Craft-Reviews"},
         {name: "Songs & Spirits", url: "https://adamlsn.github.io/songsAndSpirits/", github: "https://github.com/adamlsn/songsAndSpirits"},
-        {name: "Ecommerce Backend", url: "https://github.com/adamlsn/ecommerce", github: "https://github.com/adamlsn/ecommerce"},
         {name: "Employee Tracker", url: "https://github.com/adamlsn/employee-tracker", github: "https://github.com/adamlsn/employee-tracker"},
         {name: "Note Taker", url: "https://guarded-shore-32271.herokuapp.com/", github: "https://github.com/adamlsn/note-taker"},
         {name: "Password Generator", url: "https://adamlsn.github.io/password-gen/", github: "https://github.com/adamlsn/password-gen"}
